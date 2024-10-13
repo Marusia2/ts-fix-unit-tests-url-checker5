1664e808a4f998e22374431176939b874736115a
Marija <gestuverteja.marija@gmail.com>
2024-10-13 18:28:05 +0300

    Tl-08 homework modifications in the package.json file to run only url-checker.test.ts file

--- tests/password-checker.test.ts
+++ tests/password-checker.test.ts
@@ -1,18 +1,18 @@
 import { calculatePasswordStrength } from '../src/calculate-password-strength'
 
 // Homework 8. Password checker
-test('password strength very weak', () => {
+test('should return password strength very weak, as fulfills only length requirements', () => {
     expect(calculatePasswordStrength('password')).toBe('Very Weak');
 });
 
-test('password strength weak', () => {
+test('should return password strength weak, as fulfills  length and digits requirements', () => {
     expect(calculatePasswordStrength('password123')).toBe('Weak');
 });
 
-test('password strength moderate', () => {
+test('should return password strength moderate, as fulfills length, upper case and digits requirements', () => {
     expect(calculatePasswordStrength('Password123')).toBe('Moderate');
 });
 
-test('password strength strong', () => {
+test('shoul return password strength strong, as fulfills all mandatory requirements for strong password', () => {
     expect(calculatePasswordStrength('Password123@')).toBe('Strong');
 });
